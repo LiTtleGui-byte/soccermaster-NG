1e0a124b416e040678ad4f381eb69cf30b567422
haolinyang <haolinyang@sjtu.edu.cn>
2026-02-02 02:45:18 -0500

    sync_202

--- run_eval_siglip.sh
+++ run_eval_siglip.sh
@@ -4,13 +4,13 @@
 # 请根据你的实际情况修改以下路径
 
 # 配置文件路径
-CONFIG_PATH="configs/pretrain_base_224_multitask_aug_consine_part_temporal_early_freeze_text_extra_7000_6cards.yaml"
+CONFIG_PATH="configs/eval_siglip.yaml"
 
 # checkpoint路径（可以是文件或目录）
-CHECKPOINT_PATH="outputs/pretrain_base_224_multitask_aug_consine_part_temporal_early_freeze_text_extra_7000_6cards/epoch_8"
+CHECKPOINT_PATH="outputs/pretrain_base_224_multitask_aug_consine_part_temporal_early_freeze_text_8cards/epoch_19"
 
 # 日志目录路径（可选）
-LOG_DIR="outputs/eval_pretrain_base_224_multitask_aug_consine_part_temporal_early_freeze_text_extra_7000_6cards_epoch_8/eval_logs"
+LOG_DIR="outputs/eval_large_512_siglip2/eval_logs"
 
 # 运行evaluation
 CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 accelerate launch --num_processes=8 eval.py \
